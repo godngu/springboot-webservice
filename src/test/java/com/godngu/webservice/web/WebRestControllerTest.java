3ac06f6b0ab212be32fd1440fec5a51c7bc3a276
kil <godngu@sk.com>
2019-01-23 16:19:19 +0900

    changed and added

---
+++ src/test/java/com/godngu/webservice/web/WebRestControllerTest.java
@@ -0,0 +1,28 @@
+package com.godngu.webservice.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class WebRestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void check_profile() {
+        // when
+        String profile = restTemplate.getForObject("/profile", String.class);
+
+        // then
+        assertThat(profile).isEqualTo("local");
+    }
+}
